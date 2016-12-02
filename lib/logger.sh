e7562265e840a5def0646ec3d34fc23186c751fd
Rohan Jayaraj <rjayaraj@RJAYARAJ-E576-MPR13.local>
2016-12-01 17:26:03 -0800

    cosmetic changes

--- lib/logger.sh
+++ lib/logger.sh
@@ -14,7 +14,7 @@ function log_msg(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	echo -e "\033[34m $msg \033[0m"
 }
 
@@ -23,7 +23,7 @@ function log_msghead(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	echo -e "\033[1;34m $msg \033[0m"
 }
 
@@ -32,7 +32,7 @@ function log_info(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	echo -e "\033[36m [INFO] $msg \033[0m"
 }
 
@@ -41,7 +41,7 @@ function log_warn(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	echo -e "\033[33m [WARN] $msg \033[0m"
 }
 
@@ -50,7 +50,7 @@ function log_error(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	>&2 echo -e "\033[31m [ERROR] $msg \033[0m"
 }
 
@@ -59,7 +59,7 @@ function log_critical(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	>&2 echo -e "\033[1;41m [ERROR] $msg \033[0m"
 }
 
@@ -68,7 +68,7 @@ function log_success(){
 	if [ -z "$1" ]; then
 		return
 	fi
-	local msg=$1
+	local msg="$1"
 	echo -e "\033[1;32m [INFO] $msg \033[0m"
 }
 
