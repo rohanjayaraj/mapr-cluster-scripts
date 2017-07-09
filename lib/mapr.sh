619bf93642f37e250c8aa4215ce8114d52e80f47
Rohan Jayaraj <rjayaraj@RJAYARAJ-E576-MPR13.local>
2017-07-09 11:29:17 -0700

    adding  client traces and capture client stats

--- lib/mapr.sh
+++ lib/mapr.sh
@@ -3137,7 +3137,7 @@ function maprutil_mfsCPUUseOnCluster(){
         local tmpclog=$(mktemp)
         local loglines=$(find $alldirlist -name $fname -exec cat {} \; 2>/dev/null | sort -n)
         [ -n "$loglines" ] && echo "$loglines" | sort -n | awk '{ts=$1" "$2; cnt[ts]+=1; cmem[ts]+=$3; ccpu[ts]+=$4} END {for (i in cnt) printf("%s %.3f %.0f\n",i,cmem[i]/cnt[i],ccpu[i]/cnt[i])}' | sort -n > $tmpclog
-        while [ -s "$tmpclog" ] && [ "$clientst" -le "$clientet" ];
+        while [[ -s "$tmpclog" ]] && [[ "$clientst" -le "$clientet" ]];
         do
             echo "$(date -d "@$clientst" "+%Y-%m-%d %H:%M:%S") 0 0" >> $tmpclog
             clientst=$(date -d "@$(($clientst+1))")
