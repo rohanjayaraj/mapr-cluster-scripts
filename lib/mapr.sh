390541419cf81869b9585fc0cc5ac54cba452d95
Rohan Jayaraj <rohanj@hpe.com>
2020-08-06 13:48:17 -0700

    Ignore old 6.2.0 builds

--- lib/mapr.sh
+++ lib/mapr.sh
@@ -2019,7 +2019,7 @@ function maprutil_checkBuildExists(){
     local nodeos=$(getOSFromNode $node)
     if [ "$nodeos" = "centos" ]; then
         ssh_executeCommandasRoot "$node" "yum clean all" > /dev/null 2>&1
-        repolist=$(ssh_executeCommandasRoot "$node" "yum repolist enabled -v | grep -e Repo-id -e Repo-baseurl -e MapR | grep -A1 -B1 MapR | grep -v Repo-name | grep -iv 'mep\|opensource\|file://\|ebf' | grep Repo-baseurl | cut -d':' -f2- | tr -d " " | head -1")
+        repolist=$(ssh_executeCommandasRoot "$node" "yum repolist enabled -v | grep -e Repo-id -e Repo-baseurl -e MapR | grep -A1 -B1 MapR | grep -v Repo-name | grep -iv 'mep\|opensource\|file://\|ebf' | grep Repo-baseurl | cut -d':' -f2- | tr -d ' ' | head -1")
         retval=$(ssh_executeCommandasRoot "$node" "yum --showduplicates list mapr-core | grep $buildid")
     elif [ "$nodeos" = "ubuntu" ]; then
         ssh_executeCommandasRoot "$node" "apt-get update" > /dev/null 2>&1
@@ -2095,17 +2095,17 @@ function maprutil_getMapRVersionFromRepo(){
     local nodeos=$(getOSFromNode $node)
     local maprversion=
     if [ "$nodeos" = "centos" ]; then
-        maprversion=$(ssh_executeCommandasRoot "$node" "yum --showduplicates list mapr-core 2> /dev/null | sort -k2 | grep mapr-core | awk '{if(match(\$2,/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.[a-zA-Z]*/)) print \$0}' | grep -v "6.2.0.2018" | tail -n 1 | awk '{print \$2}' | cut -d'.' -f1-4")
+        maprversion=$(ssh_executeCommandasRoot "$node" "yum --showduplicates list mapr-core 2> /dev/null | sort -k2 | grep mapr-core | awk '{if(match(\$2,/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.[a-zA-Z]*/)) print \$0}' | grep -v "6.2.0.20" | tail -n 1 | awk '{print \$2}' | cut -d'.' -f1-4")
     elif [ "$nodeos" = "ubuntu" ]; then
         maprversion=$(ssh_executeCommandasRoot "$node" "apt-cache policy mapr-core 2> /dev/null | grep Candidate | grep -v none | awk '{print \$2}' | cut -d'.' -f1-4")
     elif [ "$nodeos" = "suse" ]; then
-        maprversion=$(ssh_executeCommandasRoot "$node" "zypper search -s mapr-core | grep -v '$curchangeset' | sort -k7 | awk '{if(match(\$7,/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.[a-zA-Z]*/)) print \$0}' | grep -v "6.2.0.2018" | tail -n 1 | awk '{print \$7}' | cut -d'.' -f1-4")
+        maprversion=$(ssh_executeCommandasRoot "$node" "zypper search -s mapr-core | grep -v '$curchangeset' | sort -k7 | awk '{if(match(\$7,/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.[a-zA-Z]*/)) print \$0}' | grep -v "6.2.0.20" | tail -n 1 | awk '{print \$7}' | cut -d'.' -f1-4")
     fi
 
     if [[ -n "$maprversion" ]]; then
         local maprv=($(echo $maprversion | tr '.' ' ' | awk '{print $1,$2}'))
         # 3 digit mapr versions
-        if [[ "${maprv[0]}" -lt "6" ]] || [[ "${maprv[0]}" -eq "6" ]] && [[ "${maprv[1]}" -le "1" ]]; then
+        if [[ "${maprv[0]}" -lt "6" ]] || [[ "${maprv[0]}" -eq "6" ]] && [[ "${maprv[1]}" -lt "2" ]]; then
             maprversion=$(echo $maprversion | cut -d'.' -f1-3)
         fi
         echo "$maprversion"
